d4fe09d352ca04ee0c61488816801cec01933685
caevh <Harrycave1234@outlook.com>
2020-10-06 14:59:34 +0100

    first commit

---
+++ FooBarDB.py
@@ -0,0 +1,23 @@
+import json 
+import os
+
+
+class FoobarDB(object):
+    def __init__(self, location):
+        self.location = os.path.expanduser(location)
+        self.load(self.location)
+    
+    def load(self, location):
+        if os.path.exists(location):
+            self._load()
+        else:
+            self.db = {}
+    
+    def _load(self):
+        self.db = json.load(open(self.location, "r"))
+    
+    def dumpdb(self):
+        try:
+            json.dump(self.db, open(self.location, "w+"))
+        except:
+            return False
